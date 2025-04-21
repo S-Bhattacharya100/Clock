6abe7818df2b1c792e2882c1ead7f2c0bebb784f
S Bhattacharya <swapnamoybhattacharya053@gmail.com>
2025-04-21 09:19:54 +0530

    Increasing responsiveness

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const hourHand = document.querySelector(".hour-hand");
-const minuteHand = document.querySelector(".minute-hand");
-const secondHand = document.querySelector(".second-hand");
-setInterval(() => {
-    const d = new Date();
-    secondHand.style.transform = `rotate(${d.getSeconds()*6}deg)`;
-    minuteHand.style.transform = `rotate(${d.getMinutes()*6}deg)`;
-    hourHand.style.transform = `rotate(${d.getHours()*30}deg)`;
-}, 1000);
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
+
+function updateClock() {
+    const now = new Date();
+        
+    const seconds = now.getSeconds() + now.getMilliseconds() / 1000;
+    const minutes = now.getMinutes() + seconds / 60 - 1; // -1 to adjust for the second hand's position
+    const hours = (now.getHours() % 12) + minutes / 60;
+    secondHand.style.transform = `translateX(-50%) rotate(${seconds * 6}deg)`;
+    minuteHand.style.transform = `translateX(-50%) rotate(${minutes * 6}deg)`;
+    hourHand.style.transform = `translateX(-50%) rotate(${hours * 30}deg)`;
+    requestAnimationFrame(updateClock);
+}
+updateClock();
